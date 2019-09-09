dd24c497441c53d4f8d8b5239b5dc5ce315cefaa
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-09 04:16:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,30 @@
 # Your code goes here!
-class Dog
+#class Dog
   
-  def initialize(bark = "woof!")
-    @bark = bark
-  end 
+#   def initialize(bark = "woof!")
+#     @bark = bark
+#   end 
 
- def name=(name)
-   @name = name
- end 
+# def name=(name)
+#   @name = name
+# end 
  
- def name
-   @name
- end
+# def name
+#   @name
+# end
  
-  def bark=(bark)
-   @bark = bark
- end 
+#   def bark=(bark)
+#   @bark = bark
+# end 
  
- def bark
-   @bark
- end
+# def bark
+#   @bark
+# end
  
-end 
+# end 
+class Dog
+def initialize(name, breed = "Mutt")
+  @name = name
+  @breed = breed
+end
+end
