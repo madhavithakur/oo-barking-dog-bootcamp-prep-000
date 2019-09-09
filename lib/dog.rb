f6703fc24dc050d3462c310a20e6d3d45165ff4a
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-09 04:19:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,31 +1,34 @@
 # Your code goes here!
-#class Dog
-  
-#   def initialize(bark = "woof!")
-#     @bark = bark
-#   end 
-
-# def name=(name)
-#   @name = name
-# end 
- 
-# def name
-#   @name
-# end
- 
-#   def bark=(bark)
-#   @bark = bark
-# end 
- 
-# def bark
-#   @bark
-# end
- 
-# end 
 class Dog
-  attr_accessor :name
+  
+  def initialize(bark = "woof!")
+    @bark = bark
+  end 
 
-  def bark
-    puts "woof!"
-  end
+def name=(name)
+  @name = name
+end 
+ 
+def name
+  @name
 end
+ 
+  def bark=(bark)
+  @bark = bark
+end 
+ 
+def bark
+  @bark
+end
+ 
+end 
+
+
+
+# class Dog
+#   attr_accessor :name
+
+#   def bark
+#     puts "woof!"
+#   end
+# end
